cff774b20b2bf0c69682aacc33ecb4ea340ac71c
GaryChiu109 <chiucheokin@gmail.com>
2024-03-04 20:03:05 +0800

    ok

--- carbon-player/app.py
+++ carbon-player/app.py
@@ -5,10 +5,10 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 #line token
-channel_access_token = '{Channel_access_token}'
-channel_secret = '{Channel_secret}'
-line_bot_api = LineBotApi(channel_access_token)
-handler = WebhookHandler(channel_secret)
+channel_access_token = 'uyT/wIyH6kkz35o7X7G8Edzgisq8l4Vn1wTvz+QMXcuKAnaXUhYucEHjaZKRXgAVnYvk3DfMhcsF60/iA6NxzaKgo0SPOb/yn7xLZxmTfzegtqB2J1na74r8SAo2aZCuBsw/+pdnfCLolxSvvD+6lwdB04t89/1O/w1cDnyilFU='
+channel_secret = 'fed72a71f8981ef1dec1e5867df85909'
+line_bot_api = LineBotApi(uyT/wIyH6kkz35o7X7G8Edzgisq8l4Vn1wTvz+QMXcuKAnaXUhYucEHjaZKRXgAVnYvk3DfMhcsF60/iA6NxzaKgo0SPOb/yn7xLZxmTfzegtqB2J1na74r8SAo2aZCuBsw/+pdnfCLolxSvvD+6lwdB04t89/1O/w1cDnyilFU=)
+handler = WebhookHandler(fed72a71f8981ef1dec1e5867df85909)
 
 app = Flask(__name__)
 
